c2bc1f21b5a568028f706947ed781dcab59e8401
FATEH BEDDIAF <62912697+madjed-hue@users.noreply.github.com>
2023-05-08 18:08:35 +0100

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & share AI prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning={true}>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <div className="app">
+            <Nav />
+            {children}
+          </div>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
